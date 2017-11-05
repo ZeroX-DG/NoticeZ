ea87a4ed4b8527924257447c01a2fbdda79ddd76
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2017-11-05 14:41:37 +0700

    Changed noticeZ

--- NoticeZ.js
+++ NoticeZ.js
@@ -1,55 +1,237 @@
-"use strict";
-
-function animateOut(a, b) {
-    a.className += " animated " + b
-}
-
-function htmlToElements(a) {
-    var temp = document.createElement('div');
-    temp.innerHTML = a;
-    return temp.firstChild;
-}
-
-function createContent(a, b, c, d, e) {
-    var f = {
-            success: "<i class='fa fa-check'></i>",
-            warning: "<i class='fa fa-times'></i>",
-            info: "<i class='fa fa-info'></i>",
-            default: ""
-        },
-        g = {
-            success: "background-color: #2ecc71; color: white;",
-            warning: "background-color: #e74c3c; color: white;",
-            info: "background-color: #3498db; color: white;",
-            default: "background-color: #bdc3c7; color: white;"
-        },
-        h = {
-            success: "30%",
-            warning: "30%",
-            info: "30%",
-            default: "0%"
-        },
-        i = "";
-    return i += "<div class='noticeBox' id='" + e + "' style='" + g[d] + ";font-family: " + c + "';z-index:1000>", i += "   <div class='left' style='width: " + h[d] + "'>", i += f[d], i += "   </div>", i += "   <div class='right'>", i += "       <span class='Ztitle'>", i += a, i += "       </span><br>", i += "       <span class='Zbody'>", i += b, i += "       </span>", i += "   </div>", i += "</div>"
-}
-
-function noticeZ(a) {
-    var b = a.title,
-        c = a.body,
-        d = void 0 != a.position ? a.position : "topRight",
-        e = void 0 != a.time ? a.time : 3e3,
-        f = void 0 != a.state ? a.state : "default",
-        g = "" + document.getElementsByClassName("noticeBox").length,
-        h = void 0 != a.animate ? a.animate : "fadeOut",
-        i = void 0 != a.font ? a.font : "Arial",
-        j = createContent(b, c, i, f, g);
-    if (j = htmlToElements(j), 0 == document.getElementsByClassName(d).length) {
-        var k = document.createElement("div");
-        k.className = "nContent " + d, k.appendChild(j), document.body.appendChild(k)
-    } else document.getElementsByClassName(d)[0].appendChild(j);
-    setTimeout(function() {
-        animateOut(document.getElementById(g), h), setTimeout(function() {
-            document.getElementsByClassName(d)[0].removeChild(document.getElementById(g))
-        }, 1010)
-    }, e)
+function NoticeZ(){
+  let showNoti = (notification) => {
+    document.body.appendChild(notification);
+  }
+  // re-arrange all the notifications when a notification is removed
+  let rearrageNotis = () => {
+    // get all the notifications that is same position to the removed noti
+    let sameTypeNoticeZClass = "NoticeZ " + options.position.split(" ")[0];
+    let notis = document.getElementsByClassName(sameTypeNoticeZClass);
+    // if the removed notification is not the last one in the list
+    if(notis.length > 0){
+      let position = options.position.split(" ");
+      let notiHeight = parseInt(notis[0].style.height.replace("px", ""));
+      let padding = options.padding;
+      for(let i = 0; i < notis.length; i++){
+        // i + 1 because the first one need index 1
+        // so that when * with padding will result = 20px
+        // otherwise, it will result 0px (stick to the top)
+        notis[i].style[position[0]] = 
+          (i * notiHeight) + (padding * (i + 1)) + "px";
+      }
+    }
+  }
+  let makeNoti = (title, content, styles, options) => {
+    let noti = document.createElement("div");
+    noti.className = "NoticeZ " + options.position.split(" ")[0];
+    for(let key in styles.notification){
+      let styleName = key;
+      let styleValue = styles.notification[key];
+      noti.style[styleName] = styleValue;
+    }
+    let titleElement = document.createElement("p");
+    let contentElement = document.createElement("p");
+    // add text to title and content
+    titleElement.innerText = title;
+    contentElement.innerText = content;
+    // apply style to title and content
+    for(let key in styles.title){
+      let styleName = key;
+      let styleValue = styles.title[key];
+      titleElement.style[styleName] = styleValue;
+    }
+    for(let key in styles.content){
+      let styleName = key;
+      let styleValue = styles.content[key];
+      contentElement.style[styleName] = styleValue;
+    }
+    // add image if specified
+    if(options.image){
+      let image = document.createElement("img");
+      image.src = options.image;
+      image.style.width = "100%";
+      image.style.height = "auto";
+      let imageContainer = document.createElement("div");
+      imageContainer.style.width = "30%";
+      imageContainer.style.height= "100%";
+      imageContainer.style.display = "inline-block";
+      imageContainer.style.cssFloat = "left";
+      imageContainer.style.marginRight = "5%";
+      imageContainer.appendChild(image);
+      noti.appendChild(imageContainer);
+    }
+    // add title and content to notification
+    let mainContainer = document.createElement("div");
+    mainContainer.style.width = "65%";
+    mainContainer.style.height= "100%";
+    mainContainer.style.display = "inline-block";
+    mainContainer.style.cssFloat = "left";
+    mainContainer.appendChild(titleElement);
+    mainContainer.appendChild(contentElement);
+    noti.appendChild(mainContainer);
+    // set the position of the notice to prevent
+    // noti overlaping
+    let sameTypeNoticeZClass = "NoticeZ " + options.position.split(" ")[0];
+    let notis = document.getElementsByClassName(sameTypeNoticeZClass);
+    let notiHeight = parseInt(noti.style.height.replace("px", ""));
+    // default variables
+    let padding = options.padding;
+    let possiblePosition = [
+      "top left",
+      "top right",
+      "bottom left",
+      "bottom right"
+    ];
+    if(possiblePosition.indexOf(options.position) != -1){
+      // correct postion
+      let position = options.position.split(" ");
+      // position[0] could be top or bottom
+      // notis.length + 1 because the first one need index 1
+      // so that when * with padding will result = 20px
+      // otherwise, it will result 0px (stick to the top)
+      noti.style[position[0]] = 
+        (notis.length * notiHeight) + (padding * (notis.length + 1)) + "px";
+      // position[1] could be left or right
+      noti.style[position[1]] = padding + "px";
+      // events
+      noti.onmouseover = events.OnMouseOver;
+      noti.onmouseleave = events.OnMouseLeave;
+      noti.ondisappear = events.OnDisappear;
+      noti.onclick = events.OnClick;
+    }
+    else{
+      console.log(
+        "[NoticeZ]: Error ! " +
+        "Invalid position '" + options.position + "'"
+      );
+      return;
+    }
+    // create a close button if noti is close-able
+    if(options.closeAble){
+      let closeBtn = document.createElement("span");
+      closeBtn.style.position = "absolute";
+      closeBtn.style.right = "10px";
+      closeBtn.style.top = "10px";
+      closeBtn.innerHTML = "&#x26CC;";
+      closeBtn.style.cursor = "pointer";
+      closeBtn.onclick = function(){
+        this.parentElement.remove();
+        rearrageNotis();
+      }
+      noti.appendChild(closeBtn);
+    }
+    return noti;
+  }
+  let title, content;
+  let styles = {
+    notification: {
+      background: "#3498db",
+      width: "350px",
+      height: "150px",
+      boxSizing: "border-box",
+      background: "#3498db",
+      padding: "20px",
+      color: "white",
+      borderRadius: "8px",
+      boxShadow: "0px 10px 20px -2px rgba(0, 0, 0, 0.24)",
+      transition: "all 0.4s",
+      position: "fixed",
+      fontFamily: "Tahoma"
+    },
+    title: {
+      fontSize: "1.5rem",
+      marginBottom: "20px",
+      fontFamily: "Tahoma"
+    },
+    content: {
+      fontSize: "1.1rem",
+      lineHeight: "1.5rem",
+      fontFamily: "Tahoma"
+    }
+  };
+  let options = {
+    position: "bottom right",
+    time: 3000,
+    padding: 10,
+    closeAble: false,
+    image: ''
+  };
+  let events = {};
+  let setOptions = (tempOptions) => {
+    options.position = tempOptions.position || options.position;
+    options.time = tempOptions.time || options.time;
+    options.closeAble = tempOptions.canClose || options.closeAble;
+    options.image = tempOptions.image || options.image;
+    // styles
+    // background
+    styles.notification.background = 
+      tempOptions.background || styles.notification.background;
+    // width
+    styles.notification.width = 
+      tempOptions.width ? tempOptions.width + "px" : 
+                          styles.notification.width;
+    // height
+    styles.notification.height = 
+      tempOptions.height ? tempOptions.height + "px" : 
+                            styles.notification.height;
+    // color
+    styles.notification.color = 
+      tempOptions.textColor || styles.notification.color;
+    // corner
+    styles.notification.borderRadius = 
+      tempOptions.roundness ? tempOptions.roundness + "px" : 
+                          styles.notification.borderRadius;
+    // font
+    styles.notification.fontFamily = 
+      tempOptions.font || styles.notification.fontFamily;
+    // titleFont
+    styles.title.fontFamily = 
+      tempOptions.titleFont || styles.title.fontFamily;
+    // contentFont
+    styles.title.fontFamily = 
+      tempOptions.contentFont || styles.title.fontFamily;
+  }
+  if (arguments.length == 2){
+    // show only title and content
+    title = arguments[0];
+    content = arguments[1];
+  }
+  else if(arguments.length == 3){
+    // title, content and options
+    title = arguments[0];
+    content = arguments[1];
+    let tempOptions = arguments[2];
+    // options
+    setOptions(tempOptions)
+  }
+  else if(arguments.length == 4){
+    // with events
+    title = arguments[0];
+    content = arguments[1];
+    let tempOptions = arguments[2];
+    setOptions(tempOptions);
+    events = arguments[3];
+  }
+  if(title && content){
+    // if we have title and content
+    // then we can show it
+    let notification = makeNoti(title, content, styles, options);
+    showNoti(notification);
+    // then hide it after a period of time
+    setTimeout(function(){
+      if(notification.ondisappear)
+        notification.ondisappear();
+      notification.remove();
+      // re-arrange notis
+      rearrageNotis();
+    }, options.time);
+  }
+  else{
+    // otherwise, the developer need
+    // to specify title and content
+    console.log(
+      "[NoticeZ]: Error ! " +
+      "Please at least provide the title and content"
+    );
+  }
 }
